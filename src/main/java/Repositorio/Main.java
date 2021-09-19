db602642dbaba751afa5effda9b67b7d7c17b75e
EDWIN JAIR DUARTE <EDWIN JAIR DUARTE@DESKTOP-0GV51BK>
2021-09-19 08:45:31 -0500

    cargando datos por primesa vez

---
+++ src/main/java/Repositorio/Main.java
@@ -0,0 +1,13 @@
+
+package Repositorio;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+     
+        System.out.println("Bienvenidos a Easyparking");
+    }
+    
+}
