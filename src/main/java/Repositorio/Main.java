e0ceee3933269c9b19de53c104981b0eb639e175
David Ortega <David Ortega@DESKTOP-TMS2O3B>
2021-09-19 09:11:40 -0500

    Probando repositorio

--- src/main/java/Repositorio/Main.java
+++ src/main/java/Repositorio/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
      
         System.out.println("Bienvenidos a Easyparking");
+        System.out.println ("Probando el repositorio");
     }
     
 }
